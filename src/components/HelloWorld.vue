91ae340a349be959357a05524f55378ab623ba37
xianru <sukey_puff@163.com>
2022-07-07 07:36:16 +0800

    vue-test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,24 @@
 <template>
-  <ol>
-    <todo-item
-      v-for="item in groceryList" 
-      v-bind:todo="item"
-      v-bind:key="item.id"
-    ></todo-item>
-  </ol>
+  <div>
+    <p>{{message}}</p>
+    <p>{{reverseMessage}}</p>
+  </div>
 </template>
 
 <script>
-import TodoItem from './TodoItem.vue'
 
 export default {
   name: 'HelloWorld',
   data() {
     return {
-      groceryList: [
-      { id: 0, text: '蔬菜' },
-      { id: 1, text: '奶酪' },
-      { id: 2, text: '随便' }
-    ]
+      message: 'Hello'
     }
   },
-  components: {
-    TodoItem
-  }
+  computed: {
+    reverseMessage: function() {
+      return this.message.split('').reverse().join('')
+    }
+  } 
 }
 </script>
 
